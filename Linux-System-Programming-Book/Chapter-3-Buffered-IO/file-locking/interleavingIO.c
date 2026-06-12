fd90a73589fa80219883e772f3640386bff5b438
pangomin <pangominn@gmail.com>
2026-06-12 20:05:04 +0330

    Add multithreading to see race condition

--- Linux-System-Programming-Book/Chapter-3-Buffered-IO/file-locking/interleavingIO.c
+++ Linux-System-Programming-Book/Chapter-3-Buffered-IO/file-locking/interleavingIO.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <pthread.h>
 
-void thread_1 (FILE *fp) {
-	fprintf(fp, "data1");
+void* thread_1 (void* arg) {
+	FILE *fp = (FILE*)arg;
+
+	// Make threads work for a period
+	for (int i = 0; i < 10; i++) {
+	fprintf(fp, "A");
+	fflush(fp);
+	usleep(200);
+	}
+	return NULL;
 }
-void thread_2 (FILE *fp) {
-	fprintf(fp, "data2");
+void* thread_2 (void *arg) {
+	FILE *fp = (FILE*)arg;
+
+	for (int i = 0; i < 10; i++) {
+	fprintf(fp, "B");
+	fflush(fp);
+	usleep(200);
+	}
+	return NULL;
 }
 
 int main(void) {
+	pthread_t t1, t2;
 	FILE *fp = fopen("data", "a");
-	thread_1(fp);
-	thread_2(fp);
+	if (fp == NULL) {
+		perror("fopen");
+		return 1;
+	}
+	pthread_create(&t1, NULL, thread_1, fp);
+	pthread_create(&t2, NULL, thread_2, fp);
+
+	// Wait till they finish their job
+	pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+	fclose(fp);
 
 	return 0;
 }
